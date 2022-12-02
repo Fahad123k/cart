da0bb01547e29f99c7e357b02ae55ac8aba3feda
Fahad123k <ahmad.nishu08@gmail.com>
2022-12-02 22:21:03 +0530

    adding setState

--- src/CartItem.js
+++ src/CartItem.js
@@ -10,7 +10,38 @@ class CartItem extends React.Component {
             qty:1,
             img:''
         }
+        
     }
+
+
+    increaseQuantity = () => {
+        console.log('this', this.state);
+        // this.setState({
+        //    qty: this.state.qty+1,
+        // })
+
+        // if prevState is require thdn
+        this.setState((prevState)=>{
+            return {
+             qty:prevState.qty+1
+            }
+         })
+      }
+       
+    decreaseQuantity = () => {
+        console.log('this', this.state);
+        // from 1
+        // this.setState({
+        //    qty: this.state.qty-1,
+        // })
+        // form 2
+        this.setState((prevState)=>{
+           return {
+            qty:prevState.qty-1
+           }
+        })
+      }
+
     render() {
         const {price ,title,qty}=this.state;
         return (
@@ -24,8 +55,18 @@ class CartItem extends React.Component {
             <div style={{color:'#777'}}> {qty}</div>
             <div className="cart-item-actions">
                 {/* buttons */}
-                <img src="https://cdn-icons-png.flaticon.com/512/992/992651.png" alt="increase" className="action-icons" />
-                <img src="https://cdn-icons-png.flaticon.com/512/992/992683.png" alt="decrease" className="action-icons" />
+                <img 
+                src="https://cdn-icons-png.flaticon.com/512/992/992651.png"
+                alt="increase" 
+                className="action-icons"
+                onClick={this.increaseQuantity}
+                />
+                <img 
+                src="https://cdn-icons-png.flaticon.com/512/992/992683.png" 
+                alt="decrease" 
+                className="action-icons" 
+                onClick={this.decreaseQuantity}
+                />
                 <img src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png" alt="delete" className="action-icons" />
             </div>
             </div>
