8441aa58eef555a0309af82a68ea10ee1f2d0407
Fahad123k <ahmad.nishu08@gmail.com>
2022-12-03 21:04:20 +0530

    dd function basd component

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,42 +1,40 @@
 import React, { Component } from 'react';
 
-class NavBar extends Component {
-    render() {
-        return (
-            <div style={styles.nav}>
-                <div style={styles.cartIconContainer}>
-                    <img style={styles.cartIcon} src="https://cdn-icons-png.flaticon.com/512/3144/3144456.png" alt="" />
-                    <span style={styles.cartCount}>3</span>
-                </div>
+const NavBar = (props) => {
+    return (
+        <div style={styles.nav}>
+            <div style={styles.cartIconContainer}>
+                <img style={styles.cartIcon} src="https://cdn-icons-png.flaticon.com/512/3144/3144456.png" alt="" />
+                <span style={styles.cartCount}>3</span>
             </div>
-        );
-    }
+        </div>
+    )
 }
 
 
 const styles = {
     cartIcon: {
-      height: 32,
-      marginRight: 20
+        height: 32,
+        marginRight: 20
     },
     nav: {
-      height: 70,
-      background: '#4267b2',
-      display: 'flex',
-      justifyContent: 'flex-end',
-      alignItems: 'center'
+        height: 70,
+        background: '#4267b2',
+        display: 'flex',
+        justifyContent: 'flex-end',
+        alignItems: 'center'
     },
     cartIconContainer: {
-      position: 'relative'
+        position: 'relative'
     },
     cartCount: {
-      background: 'yellow',
-      borderRadius: '50%',
-      padding: '4px 8px',
-      position: 'absolute',
-      right: 0,
-      top: -9
+        background: 'yellow',
+        borderRadius: '50%',
+        padding: '4px 8px',
+        position: 'absolute',
+        right: 0,
+        top: -9
     }
-  };
+};
 
 export default NavBar;
